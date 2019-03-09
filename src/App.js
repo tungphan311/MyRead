8c113c2db6137f0ebfd2229731c46073428b550c
Phan Thanh Tùng <tung@Phans-MacBook-Pro.local>
2019-03-09 13:50:10 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Book from './Book';
 
 class App extends Component {
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="app">
+        <header className="list-books-title">
+          <div>
+            <h1>My Reads</h1>
+          </div>
         </header>
+        <body className="body">
+          <h2 className="bookshelf-title">Currently Reading</h2>
+          <Book />
+        </body>
       </div>
     );
   }
---
+++ src/Book.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+const Book = (props) => {
+  return (
+    <div className="book-top">
+      <div className="book-cover">
+        
+      </div>
+    </div>
+  );
+}
+
+export default Book;
